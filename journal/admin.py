f0dc4bf44ee8251fb5c3a05ba9e7211ee7899de7
destiny <desphixs@gmail.com>
2026-05-15 02:30:50 +0100

    created the model and registered in admin

--- journal/admin.py
+++ journal/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+# We import our models so we can tell the Admin panel about them.
+from .models import Tag, Entry
 
-# Register your models here.
+# Registering a model means it will show up in the Django Admin dashboard.
+# Analogy: This is like adding a new folder to a filing cabinet so you 
+# can easily see and edit the papers inside.
+
+# We register 'Tag' so we can create new labels like #bugfix or #learning.
+admin.site.register(Tag)
+
+# We register 'Entry' so we can write journal posts and attach our tags.
+admin.site.register(Entry)
--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+# A 'Model' is like a blueprint for a database table.
+# Think of this 'Tag' model as a list of labels you can stick on things.
+# Analogy: Like a box of physical "Post-it" tags labeled 'Bugfix' or 'Learning'.
+class Tag(models.Model):
+    # 'CharField' is for short text. We limit it to 50 characters.
+    # Analogy: This is the text written on the label itself.
+    name = models.CharField(max_length=50)
+
+    # The '__str__' method tells Django how to display this object as text.
+    # Without this, it would just say "Tag object (1)" in the admin panel.
+    def __str__(self):
+        return self.name
+
+# The 'Entry' model represents a single journal post.
+# Analogy: Think of this as a single page in your physical notebook.
+class Entry(models.Model):
+    # We use 'CharField' for a short, single-line title.
+    # Analogy: The bold heading at the top of your notebook page.
+    title = models.CharField(max_length=255)
+    
+    # 'TextField' is for long paragraphs of text.
+    # Analogy: The actual body of your journal entry where you write your notes.
+    body = models.TextField()
+    
+    # 'ManyToManyField' is the magic part! It links an Entry to many Tags.
+    # One entry can have many tags, and one tag can be on many entries.
+    # 'blank=True' means you can save an entry even if you don't add any tags.
+    # Analogy: This is like having a page in your notebook where you can 
+    # stick multiple different colored labels on the corner.
+    tags = models.ManyToManyField(Tag, blank=True)
+    
+    # 'DateTimeField' with 'auto_now_add=True' sets the date automatically.
+    # Analogy: This is like a time-stamp machine that clicks the date 
+    # onto the paper the moment you finish writing and save it.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # We return the title so it's easy to identify which entry is which.
+    def __str__(self):
+        return self.title
